3d771ffe4cb2c1b39594f297659e6fb251c78c13
aankit <aankit.patel@gmail.com>
2014-10-22 21:09:17 -0400

    deleting dupe files

--- convertMono.py
+++
@@ -1,8 +0,0 @@
-#convertMono
-import os
-import scipy.io.wavfile as wf
-
-for poem in os.listdir('./wav'):
-	rate, data = wf.read("./wav/" + poem)
-	mono = data[:,0]
-	wf.write("./mono/" + poem, rate, mono)
