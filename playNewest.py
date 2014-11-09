48f2365c9758f3de94739cab77038138ef1b3e2b
aankit <aankit.patel@gmail.com>
2014-11-09 02:42:26 -0500

    for the server

--- playNewest.py
+++ playNewest.py
@@ -24,19 +24,19 @@ newestFile = newestFile[:-4]
 toPlay = '%s/%s' %(path, newestFile) 
 
 agi.stream_file(toPlay)
-agi.appexec('Wait', 2)
-result = agi.stream_file(path + '/another')
-if not result:
-	agi.verbose('no result, still waiting')
-	result = agi.wait_for_digit(5)
-agi.verbose("got digit %s" % result)
+# agi.appexec('Wait', 2)
+# result = agi.stream_file(path + '/another')
+# if not result:
+# 	agi.verbose('no result, still waiting')
+# 	result = agi.wait_for_digit(5)
+# agi.verbose("got digit %s" % result)
 
-if result.isdigit() or result=='*' or result=='#':
-	agi.set_context('test')
-	agi.set_extension('aankit')
-	agi.set_priority(4)
-	sys.exit()
-else:
-	agi.verbose("bye!")
-	agi.hangup()
-	sys.exit()
+# if result.isdigit() or result=='*' or result=='#':
+# 	agi.set_context('test')
+# 	agi.set_extension('aankit')
+# 	agi.set_priority(4)
+# 	sys.exit()
+# else:
+# 	agi.verbose("bye!")
+# 	agi.hangup()
+# 	sys.exit()
