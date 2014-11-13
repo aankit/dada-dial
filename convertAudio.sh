3414d18e620a4de316e3857b91a83aa9618bcc75
aankit <aankit.patel@gmail.com>
2014-11-13 00:28:42 -0500

    adding sox split on silence to shell

--- convertAudio.sh
+++ convertAudio.sh
@@ -6,5 +6,7 @@ do
 	output="./mono/"${file:$i}
 	echo $output
 	sox $file -c 1 -r 8000 $output
+	#we also want to cutup the file here too!
+	sox ./mono/The-Dial-A-Poem-Poets_04_waldman.wav ./cutups/${file:$i}.wav silence 1 0.2 1% 1 0.1 1% : newfile : restart
 done
 
