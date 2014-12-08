9666bee83c44029b05f548b44ca73c67bc376414
aankit <aankit.patel@gmail.com>
2014-12-08 17:04:52 -0500

    enhanced scraper app finished

--- fft_analysis.py
+++ fft_analysis.py
@@ -1,49 +1,42 @@
 import matplotlib.pyplot as plt
 from scipy.io import wavfile
-
-
-sampFreq, snd = wavfile.read('/Users/Aankit/Documents/Redial/cutups/The-Dial-A-Poem-Poets_12_kandel034.wav')
-snd = snd / (2.**15)
-
-
-import numpy
-timeArray = numpy.arange(0, snd.shape[0], 1)
-
-timeArray = timeArray / sampFreq
-timeArray = timeArray * 1000  #scale to milliseconds
-
-#ipython specific! maybe run with ipython?
-# get_ipython().magic(u'matplotlib inline')
-
-
-p = numpy.fft.fft(snd) #the FFT transform...how to speed this up? FFTW will do this in C
-n = float(len(snd)) #number of samples?
-
-import math
-nUniquePts = math.ceil((n+1)/2.0)
-p = p[0:nUniquePts] #what does this part do?
-p = abs(p)
-p = p / n
-p = p**2
-
-if n % 2 > 0:
-    p[1:len(p)] = p[1:len(p)] * 2
-else:
-    p[1:len(p) -1] = p[1:len(p) - 1] * 2  
-
-
-freqArray = numpy.arange(0, nUniquePts, 1.0) * (sampFreq / n);
-
-#analyze the file
+import numpy, math
 import pandas as pd
-df = pd.DataFrame(p, index=freqArray, columns=['power'])
 
-df_sorted = df.sort(columns='power', ascending=False)
-df_sorted.head(50)
+class harmonics(object):
+	
+	def __init__(self, file_obj):
+		sampFreq, snd = wavfile.read('/Users/Aankit/Documents/Redial/cutups/The-Dial-A-Poem-Poets_12_kandel034.wav')
+		snd = snd / (2.**15)
 
-fundamental = df[df.power==df.power.max()].index[0]
-harmonic_frequencies = numpy.arange(fundamental, 1500, fundamental)
-harmonics = df[df.index.isin(harmonic_frequencies)]
+		timeArray = numpy.arange(0, snd.shape[0], 1)
+		timeArray = timeArray / sampFreq
+		timeArray = timeArray * 1000  #scale to milliseconds
+
+		#ipython specific! maybe run with ipython?
+		# get_ipython().magic(u'matplotlib inline')
+
+		p = numpy.fft.fft(snd) #the FFT transform...how to speed this up? FFTW will do this in C
+		n = float(len(snd)) #number of samples?
+
+		nUniquePts = math.ceil((n+1)/2.0)
+		p = p[0:nUniquePts] #what does this part do?
+		p = abs(p)
+		p = p / n
+		p = p**2
+
+		if n % 2 > 0:
+		    p[1:len(p)] = p[1:len(p)] * 2
+		else:
+		    p[1:len(p) -1] = p[1:len(p) - 1] * 2  
+		freqArray = numpy.arange(0, nUniquePts, 1.0) * (sampFreq / n)
+		df = pd.DataFrame(p, index=freqArray, columns=['power'])
+		fundamental = df[df.power==df.power.max()].index[0]
+		harmonic_frequencies = numpy.arange(fundamental, 1500, fundamental)
+		harmonics = df[df.index.isin(harmonic_frequencies)]
+
+
+
 
 notes = {
 'F': 87.31,
--- retrieve_and_convert/dial-a-poems.py
+++ retrieve_and_convert/dial-a-poems.py
@@ -1,13 +1,55 @@
 #download and conversion of mp3 to wav
 from pydub import AudioSegment, silence
-import urllib, re, os, math
+from urllib import URLopener
+import re, os, math, numpy, requests
 from urlparse import urlparse
 from bs4 import BeautifulSoup
-import requests
+from scipy.io import wavfile
+import pandas as pd
+
+#file paths
+final_audio = './ubu/'
+temp = './temp/'
+
+def fft_harmonics(filepath):
+	sampFreq, snd = wavfile.read(filepath)
+	snd = snd / (2.**15)
+	timeArray = numpy.arange(0, snd.shape[0], 1)
+	timeArray = timeArray / sampFreq
+	timeArray = timeArray * 1000  #scale to milliseconds
+
+	#ipython specific! maybe run with ipython?
+	# get_ipython().magic(u'matplotlib inline')
+
+	p = numpy.fft.fft(snd) #the FFT transform...how to speed this up? FFTW will do this in C
+	n = float(len(snd)) #number of samples?
+
+	nUniquePts = math.ceil((n+1)/2.0)
+	#err....
+	p = p[0:nUniquePts] 
+	p = abs(p)
+	p = p / n
+	p = p**2
+	if n % 2 > 0:
+		p[1:len(p)] = p[1:len(p)] * 2
+	else:
+		p[1:len(p) -1] = p[1:len(p) - 1] * 2  
+	
+	freqArray = numpy.arange(0, nUniquePts, 1.0) * (sampFreq / n)
+	df = pd.DataFrame(p, index=freqArray, columns=['power'])
+	fundamental = df[df.power==df.power.max()].index[0]
+	harmonic_frequencies = numpy.arange(fundamental, 3400, fundamental) #telephony will not pick < 300Hz
+	harmonics = df[df.index.isin(harmonic_frequencies)]
+	return harmonics
 
 def cutbySilence(audio, r=1):
+	#using dBFS to normalize the silence across files
 	silence_thresh = audio.dBFS - 4/r
-	audio_splits = silence.split_on_silence(audio, min_silence_len=1000, silence_thresh=silence_thresh)
+	audio_splits = silence.split_on_silence(audio,
+	 	min_silence_len=1000, 
+	 	keep_silence=250, 
+	 	silence_thresh=silence_thresh)
+	#cuts that are still too long, maybe an area of higher overall dBFS
 	long_splits = [split for split in audio_splits if math.floor(split.duration_seconds)>15]
 	for split in long_splits:
 		audio_splits.remove(split)
@@ -15,12 +57,13 @@ def cutbySilence(audio, r=1):
 		new_splits = cutbySilence(split, r=r+1)
 		for ns in new_splits:
 			audio_splits.append(ns)
-	audio_splits = [split for split in audio_splits if math.floor(split.duration_seconds)<.5]
+	#clean the cuts of anything too short
+	audio_splits = [split for split in audio_splits if math.floor(split.duration_seconds)>.5]
 	return audio_splits
 
 def leadingZeros(num, r=1):
 	leaders = '0'*(3-len(str(num)))
-	str_num = leaders + str(num)
+	str_num = '_' + leaders + str(num)
 	return str_num
 
 
@@ -40,24 +83,38 @@ for link in links[1:2]:
 	r = requests.get(link)
 	soup = BeautifulSoup(r.text)
 	poemLinks = [a['href'] for a in soup.findAll('a',href=re.compile('http.*\.mp3'))]
-	opener = urllib.URLopener()
+	opener = URLopener()
 	for poemLink in poemLinks[:3]: 
 		print poemLink
 		#download and save file to temp file
 		filename = urlparse(poemLink).path.split('/')[-1]
-		opener.retrieve(poemLink, './temp/' + filename)
+		opener.retrieve(poemLink, temp + filename)
 		#open and convert file to mono, 8000Hz
-		poem = AudioSegment.from_mp3('./temp/' + filename)
+		poem = AudioSegment.from_mp3(temp + filename)
 		poem = poem.set_channels(1)
 		poem = poem.set_frame_rate(8000)
 		#erase temp file
-		os.remove('./temp/' + filename)
+		os.remove(temp + filename)
 		#cut the poem into lines based on silence
 		lines = cutbySilence(poem)
 		#number the lines
 		line_num = 0
-		poem_dict[filename] = []
 		for line in lines:
 			line_num += 1
-			filename = filename[:-4] + leadingZeros(linenum) + '.wav'
-			poem.export('./ubu/' + filename, format='wav')
+			line_filename = filename[:-4] + leadingZeros(line_num) + '.wav'
+			print line_filename
+			#save the lines...oh sweet golden, delicious lines
+			wave = line.export(final_audio + line_filename, format='wav')
+			#save the fundamental + harmonics info
+			poem_dict[line_filename] = fft_harmonics(final_audio + line_filename)
+
+print poem_dict
+
+
+
+
+
+
+
+
+
