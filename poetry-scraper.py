d670dd4b270f0d525a10e81e77ada3c6a68650f5
aankit <aankit.patel@gmail.com>
2014-12-09 13:14:39 -0500

    extra leading period

--- poetry-scraper.py
+++ poetry-scraper.py
@@ -92,7 +92,7 @@ for link in links[1:]:
 	if clean_link[1] == '.':
 		clean_link = link.get('href')[1:]
 	req_link = ubu_url + clean_link
-	r = requests.get(link)
+	r = requests.get(req_link)
 	soup = BeautifulSoup(r.text)
 	poemLinks = [a['href'] for a in soup.findAll('a',href=re.compile('http.*\.mp3'))]
 	opener = URLopener()
