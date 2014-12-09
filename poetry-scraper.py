166f45c99c90b09fdffdafce9433bf16ec50143b
aankit <aankit.patel@gmail.com>
2014-12-09 09:11:24 -0500

    didn't convert content length to int, missed a few

--- poetry-scraper.py
+++ poetry-scraper.py
@@ -104,7 +104,7 @@ for link in links[1:]:
 			#download and save file to temp file
 			#make sure its not massive
 			d = urllib.urlopen(poemLink)
-			if int(d.info()['Content-Length']) > 25000000:
+			if int(d.info()['Content-Length']) > 25000000: #arbitrary length, could be better
 				continue
 			filename = urlparse(poemLink).path.split('/')[-1]
 			try:
