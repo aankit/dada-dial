d270ae9966628c0a3d5a75eaa08d1b617fe7f595
aankit <aankit.patel@gmail.com>
2014-12-09 13:15:58 -0500

    printing request url for debugging

--- poetry-scraper.py
+++ poetry-scraper.py
@@ -92,6 +92,7 @@ for link in links[1:]:
 	if clean_link[1] == '.':
 		clean_link = link.get('href')[1:]
 	req_link = ubu_url + clean_link
+	print req_link
 	r = requests.get(req_link)
 	soup = BeautifulSoup(r.text)
 	poemLinks = [a['href'] for a in soup.findAll('a',href=re.compile('http.*\.mp3'))]
